fca4f0237cbc0bf5e1331ed4b0c5b07777b7b32b
B1nary_Weap0nry <68037525+inartaly@users.noreply.github.com>
2026-03-04 13:53:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const app = new Framework7({ el: '#app', theme: 'auto' });
 const dropZone = document.getElementById('drop-zone');
 const fileInput = document.getElementById('file-input');
 const outputContainer = document.getElementById('output-container');
